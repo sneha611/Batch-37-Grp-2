72d7324d6700d14930f2f27c95a802ad22ad31e8
sneha611 <snehasonawane06@gmail.com>
2025-02-03 23:37:32 +0530

    Add files via upload

---
+++ Print1To10UsingWhileLoop.java
@@ -0,0 +1,19 @@
+package WhileLoopPgm;
+
+public class Print1To10UsingWhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		
+		while (i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+
+	}
+
+}
