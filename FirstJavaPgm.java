2b04e6671ce00aa52351a3dc09b91e51d39c094d
sneha611 <snehasonawane06@gmail.com>
2025-02-03 19:30:30 +0530

    Add files via upload

---
+++ FirstJavaPgm.java
@@ -0,0 +1,18 @@
+package javademo;
+
+public class FirstJavaPgm {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+		System.out.println("Hello Batch 18");
+		System.out.println("Hello Selenium");
+		System.out.println("Hello BDD");
+	}
+	
+	
+	
+	
+	
+
+}
