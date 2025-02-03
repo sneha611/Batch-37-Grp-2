40771edbac7ffa540c3b8d914b1836263118b1a7
sneha611 <snehasonawane06@gmail.com>
2025-02-03 19:28:15 +0530

    Add files via upload

---
+++ Seleniumscript.java
@@ -0,0 +1,21 @@
+package Endurica;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Seleniumscript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver","D:\\QE Software\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");;
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://demo.opencart.com/");
+	driver.manage().window().maximize();
+	System.out.println(driver.getTitle());
+	System.out.println(driver.getCurrentUrl());
+	System.out.println(driver.getPageSource());
+	//driver.quit();
+	//Your Store
+	}
+
+}
